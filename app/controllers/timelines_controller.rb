7d2b2955d94db37e5f5b357e8cb7f114e9d63cda
k_fukuyori <ocvxkeikei@gmail.com>
2017-05-10 11:54:22 +0000

    メッセージ編集済み

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,7 +1,8 @@
 class TimelinesController < ApplicationController
   def index
     # メッセージ入力
-    @input_message = Timeline.new
+    # @input_message = Timeline.new
+    @input_message = params[:id] ? Timeline.find(params[:id]) : Timeline.new
     # タイムラインを取得
     @timeline = Timeline.includes(:user).order('updated_at DESC')
     
@@ -11,6 +12,17 @@ class TimelinesController < ApplicationController
     timeline = Timeline.new
     timeline.attributes = input_message_param
     timeline.user_id = current_user.id
+    if timeline.valid? # バリデーションチェック
+      timeline.save!
+    else
+      flash[:alert] = timeline.errors.full_messages
+    end
+    redirect_to action: :index
+  end
+  
+  def update
+    timeline = Timeline.find(params[:id])
+    timeline.attributes = input_message_param
     if timeline.valid? # バリデーションチェック
       timeline.save!
     else
